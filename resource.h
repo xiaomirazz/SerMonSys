118296b2ecb58362a7b3872d1651688682ea82bc
xiaomirazz <zhangjiawei1989@hotmail.com>
2014-10-30 20:33:40 +0800

    resource.h

---
+++ resource.h
@@ -0,0 +1,40 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by SerMonSys.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_SERMONSYS_DIALOG            102
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       129
+#define IDD_CAPABILITY                  130
+#define IDD_DISKINFO                    131
+#define IDD_SETTINGS                    132
+#define IDC_TAB1                        1003
+#define IDC_LIST2                       1008
+#define IDC_LIST3                       1009
+#define IDC_RICHEDIT                    1010
+#define IDC_STATIC2                     1012
+#define IDC_BUTTON_RESET                1013
+#define IDC_BUTTON_SAVE                 1014
+#define IDC_EDIT_MEMORY                 1015
+#define IDC_EDIT_CPU                    1016
+#define IDC_EDIT_MAIL                   1017
+#define IDC_EDIT_PWD                    1018
+#define IDC_MENU_EXIT                   32771
+#define IDC_MENU_SHUTDOWN               32772
+#define IDC_MENU_REBOOT                 32773
+#define IDC_MENU_LOGOFF                 32774
+#define IDC_MENU_ABOUT                  32775
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_CONTROL_VALUE         1019
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
