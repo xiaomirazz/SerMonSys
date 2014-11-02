7a5169bfe1e3b54c0246c3b35ef09033340198b1
xiaomirazz <zhangjiawei1989@hotmail.com>
2014-11-02 21:49:47 +0800

    sermonsysdlg.h

---
+++ SerMonSysDlg.h
@@ -0,0 +1,67 @@
+// SerMonSysDlg.h : header file
+//
+
+#if !defined(AFX_SERMONSYSDLG_H__7A654701_719D_4DEC_9A13_F9F49A53D4BD__INCLUDED_)
+#define AFX_SERMONSYSDLG_H__7A654701_719D_4DEC_9A13_F9F49A53D4BD__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "myCapap.h"
+#include "myDiskInfo.h"
+#include "mySettings.h"
+
+/////////////////////////////////////////////////////////////////////////////
+// CSerMonSysDlg dialog
+
+class CSerMonSysDlg : public CDialog
+{
+// Construction
+public:
+	CSerMonSysDlg(CWnd* pParent = NULL);	// standard constructor
+
+	//定义tab实例
+	myCapap m_capDlg;
+	myDiskInfo m_diskInfo;
+	mySettings m_settings;
+
+// Dialog Data
+	//{{AFX_DATA(CSerMonSysDlg)
+	enum { IDD = IDD_SERMONSYS_DIALOG };
+	CTabCtrl	m_TabCtrl;
+	//}}AFX_DATA
+
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CSerMonSysDlg)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	//添加自定义菜单
+	CMenu m_Menu;//定义一个菜单类变量
+
+	// Generated message map functions
+	//{{AFX_MSG(CSerMonSysDlg)
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	afx_msg void OnMenuShutdown();
+	afx_msg void OnMenuReboot();
+	afx_msg void OnMenuLogoff();
+	afx_msg void OnMenuExit();
+	afx_msg void OnMenuAbout();
+	afx_msg void OnSelchangeTab(NMHDR* pNMHDR, LRESULT* pResult);
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_SERMONSYSDLG_H__7A654701_719D_4DEC_9A13_F9F49A53D4BD__INCLUDED_)
