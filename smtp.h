73b5f1739432f0dec14719f38dba2b01b87aa619
xiaomirazz <zhangjiawei1989@hotmail.com>
2014-11-03 21:54:34 +0800

    comments

--- smtp.h
+++ smtp.h
@@ -23,7 +23,7 @@ struct data6
 	unsigned int d2:6;
 	unsigned int d1:6;
 };
-// 协议中加密部分使用的是base64方法
+
 char con628(char c6);
 void base64(char *dbuf,char *buf128,int len);
 void sendemail(char *email,char *body);
