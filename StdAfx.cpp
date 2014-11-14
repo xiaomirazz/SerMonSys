619c3c82d2f3e88941d2f3ae6ffc52f1c001f322
xiaomirazz <zhangjiawei1989@hotmail.com>
2014-11-14 22:46:15 +0800

    StdAfx.cpp

---
+++ StdAfx.cpp
@@ -0,0 +1,12 @@
+// stdafx.cpp : source file that includes just the standard includes
+//	SerMonSys.pch will be the pre-compiled header
+//	stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+CString g_cpu = "";
+CString g_memory = "";
+CString g_mail = "";
+CString g_pwd = "";
+
+
